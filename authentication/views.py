8ba1a6018b17522c07d6ccadc23eddfdb4b07f4d
Fastiman <fastiman@gmail.com>
2023-03-01 20:50:32 +0300

    added form login

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,21 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
 from django.shortcuts import render, redirect
+
+from authentication.forms import LoginForm
 
 
 def login_user(request):
-    return render(request, 'auth/login.html')
+    context = {'login_form': LoginForm()}
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('index')
+    return render(request, 'auth/login.html', context)
 
 
 def logout_user(request):
