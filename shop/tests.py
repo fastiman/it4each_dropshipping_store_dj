ed1710a32fe0a9a7f1712168769779849d903cf2
Fastiman <5336643+fastiman@users.noreply.github.com>
2023-03-05 01:05:52 +0300

    added simple tests

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,33 @@
+# python manage.py dumpdata --exclude=contenttypes --indent 4 -o data.json.gz
+# python manage.py test shop.tests --failfast
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from shop.models import Product, Payment, Order, OrderItem
+
+
+
+class TestDatabase(TestCase):
+    fixtures = [
+        "shop/fixtures/data.json"
+    ]
+
+    def setUp(self):
+        self.user = User.objects.get(username='root')
+
+    def test_user_exists(self):
+        users = User.objects.all()
+        users_number = users.count()
+        user = users.first()
+        self.assertEqual(users_number, 1)
+        self.assertEqual(user.username, 'root')
+        self.assertTrue(user.is_superuser)
+
+    def test_user_check_password(self):
+        self.assertTrue(self.user.check_password('root'))
+
+    def test_all_data(self):
+        self.assertGreater(Product.objects.all().count(), 0)
+        self.assertGreater(Order.objects.all().count(), 0)
+        self.assertGreater(OrderItem.objects.all().count(), 0)
+        self.assertGreater(Payment.objects.all().count(), 0)
