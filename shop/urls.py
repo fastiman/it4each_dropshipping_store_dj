b22c05e32b44666f4302b6e3f7e99ce4fbb27078
Fastiman <5336643+fastiman@users.noreply.github.com>
2023-03-15 16:33:53 +0300

    added cart.html

--- shop/urls.py
+++ shop/urls.py
@@ -22,7 +22,9 @@ from shop import views
 urlpatterns = [
     path('fill-database/', views.fill_database, name='fill_database'),
     path('', views.ProductsListView.as_view(), name='shop'),
-    path('cart_view/', TemplateView.as_view(template_name='shop/cart.html'), name='cart_view'),
-    path('detail/<int:pk>/',
-         TemplateView.as_view(template_name='shop/product_single.html'), name='shop_detail')
+    path('cart_view/', views.cart_view, name='cart_view'),
+    path('detail/<int:pk>/', views.ProductsDetailView.as_view(), name='shop_detail'),
+    path('add-item-to-cart/<int:pk>', views.add_item_to_cart, name='add_item_to_cart'),
+    path('delete_item/<int:pk>', views.CartDeleteItem.as_view(), name='cart_delete_item'),
+    path('make-order/', views.make_order, name='make_order'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, DetailView, DeleteView
 
-from shop.models import Product
+from shop.forms import AddQuantityForm
+from shop.models import Product, Order, OrderItem
 from shop.scraping import scraping, ScrapingError
 
 
@@ -18,3 +22,60 @@ def fill_database(request):
 class ProductsListView(ListView):
     model = Product
     template_name = 'shop/products.html'
+
+
+class ProductsDetailView(DetailView):
+    model = Product
+    template_name = 'shop/product_single.html'
+
+
+@login_required(login_url=reverse_lazy('login'))
+def add_item_to_cart(request, pk):
+    if request.method == 'POST':
+        quantity_form = AddQuantityForm(request.POST)
+        if quantity_form.is_valid():
+
+            quantity = quantity_form.cleaned_data['quantity']
+            if quantity:
+                cart = Order.get_cart(request.user)
+                product = get_object_or_404(Product, pk=pk)
+                cart.orderitem_set.create(product=product,
+                                          quantity=quantity,
+                                          price=product.price)
+                cart.save()
+                print(Order)
+                return redirect('cart_view')
+        else:
+            pass
+        return redirect('shop')
+
+
+@login_required(login_url=reverse_lazy('login'))
+def cart_view(request):
+    cart = Order.get_cart(request.user)
+    items = cart.orderitem_set.all()
+    context = {
+        'cart': cart,
+        'items': items
+    }
+    return render(request, 'shop/cart.html', context)
+
+
+@method_decorator(login_required, name='dispatch')
+class CartDeleteItem(DeleteView):
+    model = OrderItem
+    template_name = 'shop/cart.html'
+    success_url = reverse_lazy('cart_view')
+
+    # Проверка доступа
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs.filter(order__user=self.request.user)
+        return qs
+
+
+@login_required(login_url=reverse_lazy('login'))
+def make_order(request):
+    cart = Order.get_cart(request.user)
+    cart.make_order()
+    return redirect('shop')
